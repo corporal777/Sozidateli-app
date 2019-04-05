fce6e60b2d2f5103381cc4f1eb1986cda2cc68c5
Anton Nazarov <95toxa95@gmail.com>
2019-04-05 14:16:30 +0300

    uncomment in fragmentBuilderModule

--- app/src/main/java/com/example/di/FragmentBuildersModule.kt
+++ app/src/main/java/com/example/di/FragmentBuildersModule.kt
@@ -43,6 +43,7 @@ import com.example.ui.speakers.event.EventSpeakersFragment
 import com.example.ui.speakers.favorite.FavoriteSpeakersFragment
 import com.example.ui.splash.SplashFragment
 import com.example.ui.subevent.SubeventFragment
+import com.example.ui.subevent.users.SubeventUserListFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -179,6 +180,6 @@ abstract class FragmentBuildersModule {
     @ContributesAndroidInjector
     abstract fun contributeSubeventFragment(): SubeventFragment
 
-    /*@ContributesAndroidInjector
-    abstract fun contributeUserListFragment(): SubeventUserListFragment*/
+    @ContributesAndroidInjector
+    abstract fun contributeUserListFragment(): SubeventUserListFragment
 }
