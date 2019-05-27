551bab31d168963acbd65b461e187cf0286e214f
Anton Nazarov <95toxa95@gmail.com>
2019-05-27 13:56:25 +0300

    add no connect error handle

--- app/src/main/java/com/example/util/FcmMessagingService.kt
+++ app/src/main/java/com/example/util/FcmMessagingService.kt
@@ -10,6 +10,7 @@ import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import dagger.android.AndroidInjection
 import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import performOnBackgroundOutOnMain
 import javax.inject.Inject
 
@@ -52,14 +53,17 @@ class FcmMessagingService : FirebaseMessagingService() {
         val senderId = userChat.userSender?.user_id ?: return
         val senderName = userChat.userSender?.fullName ?: return
 
-        chatNotificationHelper.showNotificationIfCan(
-                chatId = chatId,
-                messageId = messageId,
-                message = message,
-                senderId = senderId,
-                senderName = senderName,
-                avatarUrl = userChat.userSender?.user_avatar
-        )
+        Completable.fromAction {
+            chatNotificationHelper.showNotificationIfCan(
+                    chatId = chatId,
+                    messageId = messageId,
+                    message = message,
+                    senderId = senderId,
+                    senderName = senderName,
+                    avatarUrl = userChat.userSender?.user_avatar
+            )
+        }.subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe()
     }
 
     companion object {
