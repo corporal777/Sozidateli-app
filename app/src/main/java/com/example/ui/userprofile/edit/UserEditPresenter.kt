96ca91043880bbf621fb64b374340e8ed4d0bd31
MAN4GRAY <prokopchukserhii@gmail.com>
2021-03-11 18:12:27 +0200

    fixed crash on aplha

--- app/src/main/java/com/example/ui/userprofile/edit/UserEditPresenter.kt
+++ app/src/main/java/com/example/ui/userprofile/edit/UserEditPresenter.kt
@@ -68,7 +68,7 @@ class UserEditPresenter
                                     .performOnBackgroundOutOnMain()
                                     .subscribe({ add ->
                                         if (add.isNotEmpty())
-                                            user.user_short_address = add[0].region
+                                            user.address?.shortAddres = add[0].region
                                         if (BuildConfig.NEW_PROFILE_EDIT) {
                                             setPersonalDataNew(user)
                                         } else {
